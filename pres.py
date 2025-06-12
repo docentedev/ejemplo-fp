bef431edc1af1edc1a377f9242529fb915de9783
Claudio <52985299+docentedev@users.noreply.github.com>
2025-06-12 10:40:41 -0400

    Update pres.py

--- pres.py
+++ pres.py
@@ -1,1 +1,28 @@
-print('hola!')
+usuario = {
+    'id': 12,
+    'name': 'Carlos',
+}
+print(f'El nombre es: {usuario['name']}')
+
+numeros = []
+opt = ''
+while opt != 'x':
+    opt = input('Ingrese una opción (sumar, res, x para salir): ')
+    if opt == 'sumar':
+        print('Sumando...')
+        nuevo_num = 0.0
+        while True:
+            try:
+                nuevo_num = float(input('ingrese num: '))
+                break
+            except ValueError:
+                print('Valor incorrecto...')
+        numeros.append(nuevo_num)
+        print(numeros)
+    if opt == 'res':
+        print('Resultado')
+        resultado = sum(numeros)
+        promedio = resultado / len(numeros)
+        print(f'El resultado es: {resultado}')
+        print(f'El promedio: {promedio}')
+
